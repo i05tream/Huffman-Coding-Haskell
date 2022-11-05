51a04165d25d360c6d1f44d4e4ccb75d24b829d9
wak-io <wak-io@wak-io.com>
2022-11-06 01:24:42 +0900

    implement orderByFrequency

--- src/Data/Huffman.hs
+++ src/Data/Huffman.hs
@@ -6,3 +6,8 @@ genFrequencyMap :: String -> Map.Map Char Integer
 genFrequencyMap cs =
   let cs' = zip cs $ repeat 1
    in Map.fromListWith (+) cs'
+
+orderByFrequency :: String -> String
+orderByFrequency cs =
+  let m = genFrequencyMap cs
+   in Prelude.map fst . assocs $ m
