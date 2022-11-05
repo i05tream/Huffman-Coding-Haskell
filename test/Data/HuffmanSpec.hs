eb86815076b7eabd7eebbba0cb4234a9ed54b4cf
wak-io <wak-io@wak-io.com>
2022-11-06 00:49:33 +0900

    Add testcase for genFrequencyMap

---
+++ test/Data/HuffmanSpec.hs
@@ -0,0 +1,12 @@
+module Data.HuffmanSpec where
+
+import Data.Huffman
+import Data.Map.Strict as Map
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "genFrequencyMap" $ do
+    it "returns the Map of frequency of characters" $ do
+      let result = Map.fromList [('A', 3), ('B', 2), ('C', 1)]
+      genFrequencyMap "ABCABA" `shouldBe` result
