2d028a631cdd27249224dfa5de13f6e2ba7b1112
wak-io <wak-io@wak-io.com>
2022-11-07 09:59:21 +0900

    add testcase for initialFrequency

--- test/Data/HuffmanSpec.hs
+++ test/Data/HuffmanSpec.hs
@@ -2,6 +2,7 @@ module Data.HuffmanSpec where
 
 import Data.Huffman
 import Data.Map.Strict as Map
+import Data.Tree (Tree (Node))
 import Test.Hspec
 
 spec :: Spec
@@ -10,6 +11,11 @@ spec = do
     it "returns the Map of frequency of characters" $ do
       let result = Map.fromList [('A', 3), ('B', 2), ('C', 1)]
       genFrequencyMap "ABCABA" `shouldBe` result
-  describe "orderByFrequency" $ do
-    it "sort characters by frequency" $ do
-      orderByFrequency "AAAABBBCCD" `shouldBe` "ABCD"
+  describe "initialFrequency" $ do
+    it "returns List of leaves" $ do
+      let result =
+            [ (Node (SingleChar 'd' 3) [])
+            , (Node (SingleChar 'e' 2) [])
+            , (Node (SingleChar 'f' 1) [])
+            ]
+      initialFrequency "dedfed" `shouldBe` result
