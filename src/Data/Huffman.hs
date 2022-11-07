cddd5fa7f4ebedbce63b0b9ecc60f8e94871b275
wak-io <wak-io@wak-io.com>
2022-11-07 10:14:02 +0900

    add implementation to genHuffmanTree

--- src/Data/Huffman.hs
+++ src/Data/Huffman.hs
@@ -3,7 +3,7 @@ module Data.Huffman where
 import Data.Map.Strict as Map
 import Data.Tree
 
-data HuffmanTreeNode = SingleChar Char Int deriving (Show, Eq)
+data HuffmanTreeNode = SingleChar Char Int | Sum Int deriving (Show, Eq)
 
 genFrequencyMap :: String -> Map.Map Char Int
 genFrequencyMap cs =
@@ -14,3 +14,6 @@ initialFrequency :: String -> [Tree HuffmanTreeNode]
 initialFrequency cs =
   let freqs = genFrequencyMap cs
    in Map.elems . Map.mapWithKey (\k v -> Node (SingleChar k v) []) $ freqs
+
+genHuffmanTree :: [Tree HuffmanTreeNode] -> Tree HuffmanTreeNode
+genHuffmanTree [t] = t
